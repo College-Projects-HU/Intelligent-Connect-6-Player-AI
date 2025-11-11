c1a391322a659175e010f04e2f8ca2405e7464ee
Ahmed Tharwat <ahmedlordexg@gmail.com>
2025-11-11 21:47:00 +0200

    Alpha-Beta First

---
+++ src/alpha_beta.py
@@ -0,0 +1,126 @@
+import game_logic as connect6
+import math
+def alpha_beta(game, depth, alpha, beta, maximizing_player):
+    win = game.check_win(game.last_row, game.last_col)
+    if win[0] and win[1] == connect6.AI_PLAYER:
+        return math.inf
+    elif win[0] and win[1] == connect6.PLAYER_1:
+        return -math.inf
+    elif depth == 0 or game.check_draw():
+        return 0 # 3ayz evaluation function
+
+
+    if maximizing_player:
+        max_score = -math.inf
+        orig_r, orig_c = game.last_row, game.last_col
+        outer_break = False  # For pruning
+
+        for (x1, y1) in game.get_all_legal_moves():
+            game.board[x1][y1] = connect6.AI_PLAYER  # SET FIRST STONE
+            game.last_row = x1
+            game.last_col = y1
+
+            for (x2, y2) in game.get_all_legal_moves():
+                # SET SECOND STONE
+                game.board[x2][y2] = connect6.AI_PLAYER
+                game.last_row = x2
+                game.last_col = y2
+
+
+                # What's going to happen from now on?
+                score = alpha_beta(game, depth - 1, alpha, beta, False)
+
+                # Reset second stone
+                game.board[x2][y2] = connect6.EMPTY
+                # Restore state to *after move 1*
+                game.last_row = x1
+                game.last_col = y1
+
+                # MAX score and pruning
+                max_score = max(max_score, score)
+                alpha = max(alpha, score)
+                if beta <= alpha:
+                    outer_break = True
+                    break
+            # Reset first stone
+            game.board[x1][y1] = connect6.EMPTY
+
+            if outer_break: break
+
+        game.last_row = orig_r
+        game.last_col = orig_c
+        return max_score
+    else:
+        min_score = math.inf
+        # 1. Save the state *once*
+        orig_r, orig_c = game.last_row, game.last_col
+
+        outer_break = False
+        for (x1, y1) in game.get_all_legal_moves():
+            game.board[x1][y1] = connect6.PLAYER_1
+            game.last_row = x1
+            game.last_col = y1
+
+            for (x2, y2) in game.get_all_legal_moves():
+                game.board[x2][y2] = connect6.PLAYER_1
+                game.last_row = x2
+                game.last_col = y2
+
+                score = alpha_beta(game, depth - 1, alpha, beta, True)
+
+                # Undo move 2
+                game.board[x2][y2] = connect6.EMPTY
+                # Restore state to *after move 1*
+                game.last_row = x1
+                game.last_col = y1
+
+                min_score = min(min_score, score)
+                beta = min(beta, score)
+                if beta <= alpha:
+                    outer_break = True
+                    break
+
+            # Undo move 1
+            game.board[x1][y1] = connect6.EMPTY
+            if outer_break:
+                break
+
+        # 2. Restore the *original* state
+        game.last_row = orig_r
+        game.last_col = orig_c
+        return min_score
+
+
+def find_best_move(game):
+    best_score = -math.inf
+    best_move = None
+    orig_r, orig_c = game.last_row, game.last_col
+    for (x1, y1) in game.get_all_legal_moves():
+        game.board[x1][y1] = connect6.AI_PLAYER # SET FIRST STONE
+        game.last_row = x1
+        game.last_col = y1
+
+        for (x2, y2) in game.get_all_legal_moves():
+            game.board[x2][y2] = connect6.AI_PLAYER # SET SECOND STONE
+            game.last_row = x2
+            game.last_col = y2
+
+            # What's going to happen from now on?
+            score = alpha_beta(game, game.depth, -math.inf, math.inf, False)
+
+            if score >= best_score:
+                best_score = score
+                best_move = [(x1, y1), (x2, y2)]
+
+            # Reset second stone
+            game.board[x2][y2] = connect6.EMPTY
+            # Restore state to *after move 1*
+            game.last_row = x1
+            game.last_col = y1
+        # Reset first stone
+        game.board[x1][y1] = connect6.EMPTY
+        # 2. Restore the *original* state
+        game.last_row = orig_r
+        game.last_col = orig_c
+
+    return best_move
